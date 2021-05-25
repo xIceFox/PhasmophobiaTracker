7775f6055a058e65c7f423c2ff42274795c0e9f6
Konjiki No Yami <yamidoesdev@gmail.com>
2021-05-25 20:21:36 +0200

    Changed the base design & added language switcher

---
+++ js/dataObjectives.js
@@ -0,0 +1,30 @@
+const OBJECTIVES = {
+    "de": [
+        "Wählen Sie eine Option",
+        "Lassen Sie ein Teammitglied Zeuge eines Geisterevents werden",
+        "Machen Sie ein Foto vom Geist",
+        "Finden Sie Beweise für übernatürliche Ereignisse mit einem EMF Reader",
+        "Stellen Sie die Anwesenheit eines Geistes anhand eines Bewegungssensors fest",
+        "Reinigen Sie den Bereich in der Nähe des Geistes mit einem Räucherstäbchen",
+        "Hindern Sie den Geist mit einem Kruzifix daran, Sie heimzusuchen",
+        "Lassen Sie einen Geist durch Salz wandern",
+        "Erkennen Sie eine Geisterpräsenz mit einer Kerze",
+        "Entkommen Sie dem Geist während einer Jagd ohne Todesfälle",
+        "Verwenden Sie Smudge Sticks, während der Geist einen Spieler jagt",
+        "Erhalten Sie eine durchschnittliche geistige Gesundheit unter 25%"
+    ],
+    "en": [
+        "Choose an Option",
+        "Have a member of your team witness a Ghost Event",
+        "Capture a photo of the Ghost",
+        "Find evidence of the paranormal with an EMF Reader",
+        "Detect a Ghosts presence with a Motion Sensor",
+        "Cleanse the area near the Ghost using Smudge Sticks",
+        "Prevent the Ghost from hunting with a Crucifix",
+        "Get a Ghost to walk through Salt",
+        "Detect a Ghost presence with a Candle",
+        "Escape the Ghost during a Hunt with no deaths",
+        "Use Smudge Sticks while the Ghost is hunting a player",
+        "Get an average Sanity below 25%"
+    ]
+}
--- js/index.js
+++ js/index.js
@@ -1,11 +1,38 @@
+let langSelector = document.querySelector("#langSwitch")
+langSelector.addEventListener("click", toggleLanguage)
+
 let taskElements = document.querySelectorAll(".task")
 taskElements.forEach(createHTMLSelector)
 
 function createHTMLSelector(_currentSelect,index) {
-    for (let i = 0; i < OBJECTIVES.length; i++) {
+    _currentSelect.innerHTML = ""
+    let tasks = chooseTasks();
+    for (let i = 0; i < tasks.length; i++) {
         let option = document.createElement("option")
         option.setAttribute("value", i)
-        option.appendChild(document.createTextNode(OBJECTIVES[i]))
+        option.innerHTML = tasks[i]
         _currentSelect.appendChild(option)
     }
 }
+
+function chooseTasks() {
+    let activeLang = langSelector.dataset.lang;
+    if (activeLang === "de") return OBJECTIVES.de
+    else return OBJECTIVES.en
+}
+
+function toggleLanguage() {
+    let activeLang = langSelector.dataset.lang;
+    if (activeLang === "de") {
+        langSelector.innerHTML = "DE"
+        langSelector.dataset.lang = "en"
+        taskElements.forEach(createHTMLSelector)
+    }
+    else if (activeLang === "en") {
+        langSelector.innerHTML = "EN"
+        langSelector.dataset.lang = "de"
+        taskElements.forEach(createHTMLSelector)
+    }
+}
+
+
